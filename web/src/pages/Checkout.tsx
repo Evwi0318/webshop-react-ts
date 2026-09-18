28a4358764d635c2584e4e7ad72d611fc32c5f30
Arnell <Arnell.1996@hotmail.com>
2026-09-18 14:57:09 +0200

    feat: Bygg ut Checkout-sidan med layout och submit-logik

--- web/src/pages/Checkout.tsx
+++ web/src/pages/Checkout.tsx
@@ -1,5 +1,48 @@
 import { Title } from "@mantine/core";
+import { CartList } from "../components/cart/CartList";
+import { CartSummary } from "../components/cart/CartSummary";
+import { useCart } from "../hooks/useCart";
+import { useNavigate } from "react-router-dom";
+import { CheckoutForm } from "../components/Checkout/CheckoutForm";
+import { createOrder } from "../lib/api";
+
+
 
 export function Checkout() {
-  return <Title order={1}>Kassa</Title>;
+  const {items, clearCart} = useCart();
+  const navigate = useNavigate();
+
+  if(items.length === 0){
+    return(
+      <div>
+        <Title order={1}>Tomt här,lägg till produkter!</Title>
+        <a href="/">Gå till startsidan</a>
+      </div>
+    );
+  }
+
+const handleCheckoutSubmit = async (formData:any) =>
+{
+  const order = await createOrder({
+    items: items.map(i => ({productId: i.product.id, quantity: i.quantity})),
+    customer: formData,
+  });
+  clearCart ();
+  navigate(`/confirmation/${order.orderNumber}`, {replace: true});
+}
+
+  return (
+    <div>
+      <Title order={1}>Kassa</Title>
+      <div style={{display: "flex", gap: "2rem"}}>
+        <div style={{flex: 1}}>
+          <CartList/>
+          <CartSummary/>
+        </div>
+        <div style={{flex: 1}}>
+        <CheckoutForm onSubmit={handleCheckoutSubmit}/>
+        </div>
+      </div>
+    </div>
+  );
 }
